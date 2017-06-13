b48af7f5fddb77c18a7ea02418bcb03b531904eb
Gabrielle Campagna <gabycampagna@gmail.com>
2017-06-13 15:30:28 -0700

    Add pie-chart

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -12,5 +12,23 @@ export default Controller.extend({
       label: 'Very Cool',
       data: 200
     },
+  ],
+  pieData: [
+    {
+      label: 'Totally Cool',
+      data: 300
+    },
+    {
+      label: 'Way Cool',
+      data: 200
+    },
+    {
+      label: 'Cucumber Cool',
+      data: 400
+    },
+    {
+      label: 'So Cool',
+      data: 500
+    },
   ]
 });
