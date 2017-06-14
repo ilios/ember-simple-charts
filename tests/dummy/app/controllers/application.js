ead6714a91bd6b76e48df46f732ee17524c07952
Gabrielle Campagna <gabycampagna@gmail.com>
2017-06-14 16:50:31 -0700

    Add Line chart

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -82,5 +82,31 @@ export default Controller.extend({
       label: 'Jason',
       data: 350
     }
+  ],
+  lineData: [
+    {
+      label: '0',
+      data: 0
+    },
+    {
+      label: '10',
+      data: 20
+    },
+    {
+      label: '20',
+      data: 30
+    },
+    {
+      label: '30',
+      data: 60
+    },
+    {
+      label: '40',
+      data: 70
+    },
+    {
+      label: '70',
+      data: 90
+    }
   ]
 });
