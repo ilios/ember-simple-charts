43c4cc0ef7470501277539266c0d3ad981eb4769
Gabrielle Campagna <gabycampagna@gmail.com>
2017-06-27 19:15:05 -0700

    Add new handlebar templates for charts

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,6 +7,16 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('donut-chart');
+  this.route('pie-chart');
+  this.route('line-chart');
+  this.route('chart-donut');
+  this.route('chart-pie');
+  this.route('chart-line');
+  this.route('chart-legend');
+  this.route('chart-horz-bar');
+  this.route('chart-stacked');
+  this.route('chart-bar');
 });
 
 export default Router;
