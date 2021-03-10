517ffbeaf149d51d3b032a16d75a34ad6c9b096a
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-10 15:53:26 -0800

    Move docs stuff from test app to docs app

--- docs/app/router.js
+++ docs/app/router.js
@@ -1,9 +1,20 @@
-import EmberRouter from '@ember/routing/router';
 import config from 'simple-charts-docs/config/environment';
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 
-export default class Router extends EmberRouter {
+export default class Router extends AddonDocsRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  docsRoute(this, function () {
+    this.route('donut');
+    this.route('pie');
+    this.route('bar');
+    this.route('horz-bar');
+    this.route('cluster');
+    this.route('pack');
+    this.route('tree');
+  });
+  this.route('not-found', { path: '/*path' });
+});
--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -2,9 +2,11 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-addon-docs': {
+      documentingAddonAt: '..'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
