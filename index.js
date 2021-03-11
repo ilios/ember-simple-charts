105056e89cee772f4b102eeb6e010629a70e5de6
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-10 21:38:19 -0800

    Remove unneeded config and dependencies

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-const merge = require('merge');
-
 module.exports = {
   name: require('./package').name,
-  config: function (environment, appConfig) {
-    const initialConfig = merge({}, appConfig);
-    const updatedConfig = this.addons.reduce((config, addon) => {
-      if (addon.config) {
-        merge(config, addon.config(environment, config));
-      }
-      return config;
-    }, initialConfig);
-    return updatedConfig;
-  },
 };
