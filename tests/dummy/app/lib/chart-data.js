5b9f8260b7017a97940c7d5eac952c6da65e805a
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-10 21:01:02 -0800

    Add some examples and test with percy
    This is a much paired down version of our docs useful for development
    and testing.

---
+++ tests/dummy/app/lib/chart-data.js
@@ -0,0 +1,285 @@
+// BEGIN-SNIPPET chart-data.js
+const donut = [
+  {
+    label: 'Totally Cool',
+    data: 300,
+  },
+  {
+    label: 'Way Cool',
+    data: 200,
+  },
+  {
+    label: 'Cucumber Cool',
+    data: 400,
+  },
+  {
+    label: 'So Cool',
+    data: 500,
+  },
+];
+const pie = [
+  {
+    label: 'Totally Cool',
+    data: 300,
+  },
+  {
+    label: 'Way Cool',
+    data: 200,
+  },
+  {
+    label: 'Cucumber Cool',
+    data: 400,
+  },
+  {
+    label: 'So Cool',
+    data: 500,
+  },
+];
+const bar = [
+  {
+    label: '300',
+    data: 300,
+  },
+  {
+    label: '200',
+    data: 200,
+  },
+  {
+    label: '400',
+    data: 400,
+  },
+  {
+    label: 'Long Label',
+    data: 500,
+  },
+];
+const horzBar = [
+  {
+    label: 'Mark',
+    data: 150,
+  },
+  {
+    label: 'John',
+    data: 200,
+  },
+  {
+    label: 'Kathy',
+    data: 300,
+  },
+  {
+    label: 'Jeff Long Namerson',
+    data: 350,
+  },
+  {
+    label: 'Joe',
+    data: 100,
+  },
+  {
+    label: 'Kelly',
+    data: 200,
+  },
+  {
+    label: 'Jason',
+    data: 350,
+  },
+];
+const cluster = {
+  name: 'Root',
+  children: [
+    {
+      name: 'first one',
+      children: [
+        {
+          name: 'first two one',
+          children: [],
+        },
+        {
+          name: 'first two two',
+          children: [],
+        },
+        {
+          name: 'first two three',
+          children: [],
+        },
+      ],
+    },
+    {
+      name: 'second one',
+      children: [
+        {
+          name: 'second two one',
+          children: [
+            {
+              name: 'second three one',
+              children: [
+                {
+                  name: 'second four one',
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+const pack = {
+  label: 'Root',
+  children: [
+    {
+      label: 'first one',
+      children: [
+        {
+          label: 'first two one',
+          value: 8,
+          children: [],
+        },
+        {
+          label: 'first two two',
+          value: 1,
+          children: [],
+        },
+        {
+          label: 'first two three',
+          value: 2,
+          children: [],
+        },
+      ],
+    },
+    {
+      label: 'second one',
+      children: [
+        {
+          label: 'second two one',
+          value: 30,
+          children: [
+            {
+              label: 'second three one',
+              value: 20,
+              children: [
+                {
+                  label: 'second four one',
+                  value: 10,
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'third one',
+      value: 133,
+      children: [],
+    },
+    {
+      label: 'fourth one',
+      children: [
+        {
+          label: 'fourth two one',
+          value: 20,
+          children: [],
+        },
+        {
+          label: 'fourth two two',
+          value: 13,
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+const tree = {
+  label: 'Root',
+  children: [
+    {
+      label: 'first one',
+      children: [
+        {
+          label: 'first two one',
+          value: 8,
+          children: [],
+        },
+        {
+          label: 'first two two',
+          value: 1,
+          children: [],
+        },
+        {
+          label: 'first two three',
+          value: 2,
+          children: [],
+        },
+      ],
+    },
+    {
+      label: 'second one',
+      children: [
+        {
+          label: 'second two one',
+          value: 30,
+          children: [
+            {
+              label: 'second three one',
+              value: 20,
+              children: [
+                {
+                  label: 'second four one',
+                  value: 10,
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'third one',
+      value: 133,
+      children: [],
+    },
+    {
+      label: 'fourth one',
+      children: [
+        {
+          label: 'fourth two one',
+          value: 20,
+          children: [],
+        },
+        {
+          label: 'fourth two two',
+          value: 13,
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+
+const click = function (data) {
+  alert(data.label || data.name);
+};
+
+const ipsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget nunc cursus,
+  efficitur tortor in, varius lacus. Praesent odio tellus, sollicitudin tincidunt luctus sit amet, dignissim nec risus.
+  Aenean pharetra laoreet blandit. Suspendisse consectetur justo eu elit vestibulum, a suscipit ante lacinia.
+  Nulla dapibus massa tortor, non fermentum nisl efficitur commodo. Mauris pretium odio et lobortis viverra.
+  Nullam consequat tortor ut leo bibendum viverra. Pellentesque purus arcu, pretium non consectetur in,
+  tempus vel eros. Suspendisse potenti. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+  Donec eget fringilla felis. Proin sit amet nisi turpis.`;
+
+export default {
+  donut,
+  pie,
+  bar,
+  horzBar,
+  ipsum,
+  cluster,
+  pack,
+  tree,
+  click,
+};
+// END-SNIPPET
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,20 +1,9 @@
-import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import EmberRouter from '@ember/routing/router';
 import config from 'dummy/config/environment';
 
-export default class Router extends AddonDocsRouter {
+export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {
-  docsRoute(this, function () {
-    this.route('donut');
-    this.route('pie');
-    this.route('bar');
-    this.route('horz-bar');
-    this.route('cluster');
-    this.route('pack');
-    this.route('tree');
-  });
-  this.route('not-found', { path: '/*path' });
-});
+Router.map(function () {});
