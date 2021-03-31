4b51a7e85b41a06e30ced123d02d4d7688f7d9ad
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-31 16:07:47 -0700

    Add more testable stuff
    This makes it easier to see in the test app what is working by adding
    hover and click.

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { capitalize, camelize } from '@ember/string';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
   @service chartData;
+  @tracked tooltipData;
   chartTypes = ['bar', 'cluster', 'donut', 'horz-bar', 'pack', 'pie', 'tree'];
   get charts() {
     return this.chartTypes.map((name) => {
@@ -13,5 +16,12 @@ export default class ApplicationController extends Controller {
         data: camelize(name),
       };
     });
+  }
+  click({ label, data }) {
+    alert(`Clicked "${label}: ${data}"`);
+  }
+  @action
+  setToolTipData(data) {
+    this.tooltipData = data;
   }
 }
