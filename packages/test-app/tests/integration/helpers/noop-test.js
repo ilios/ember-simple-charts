a31aa8a1cc95f2026bb5ec1cc424e0cdca69f349
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-11-18 15:33:50 -0800

    adds noop helper drop-in replacement.

---
+++ packages/test-app/tests/integration/helpers/noop-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'test-app/tests/helpers';
+import { click, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | noop', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('nothing happens when invoked', async function (assert) {
+    assert.expect(0);
+    await render(hbs`<button {{@on-click (noop)}}></button>`);
+    await click('button');
+  });
+});
