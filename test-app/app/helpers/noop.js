643d3fc1fd383173b79e4285798115b6f1f1c3b1
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-02-10 23:03:09 -0800

    Drop the Test App
    This pattern is going away, tests are moving back into the addon.

--- test-app/app/helpers/noop.js
+++
@@ -1,6 +0,0 @@
-// taken from Ember Composable Helpers (https://github.com/DockYard/ember-composable-helpers)
-import { helper } from '@ember/component/helper';
-
-export default helper(function noop() {
-  return () => {};
-});
