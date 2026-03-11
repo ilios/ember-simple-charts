c457ab37eed4a6014f51aaff07de629945ae0fd2
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-03-11 09:35:17 -0700

    Install Ember Concurrency in Docs App
    This shouldn't be needed, but trying it anyway.

--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -7,6 +7,13 @@ module.exports = function (defaults) {
     autoImport: {
       watchDependencies: ['ember-simple-charts'],
     },
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+
+        // NOTE: put any code coverage plugins last, after the transform.
+      ],
+    },
   });
   app.import('node_modules/highlight.js/styles/a11y-dark.css');
 
